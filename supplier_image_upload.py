5309cff93918c6125c66f6a8a9e53b6265fcef22
Subhodeep <subhodeep.s2013@gmail.com>
2020-08-10 12:47:59 +0530

    Edited supplier_image_upload.py
    Added code to the script. The script is all good but this won't work because
    the url is a dummy. It will only work in the Google platform.

---
+++ supplier_image_upload.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import requests
+import os
+
+url="http://localhost/upload"
+directory_path="supplier-data/images" #Where the .jpeg images are stored
+for each_file in os.listdir(directory_path):
+    if ".jpeg" in each_file:
+        with open("{}/{}".format(directory_path,each_file),"rb") as file:
+            r = requests.post(url,files={'file':file})
