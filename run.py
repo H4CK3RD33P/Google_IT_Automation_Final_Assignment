46fa6589c2071bd5d2c747a32b3d8c538b26ac11
Subhodeep <subhodeep.s2013@gmail.com>
2020-08-10 18:07:35 +0530

    Added new feature to run.py
    Now I've built a function that processes the text files, makes it a dict, appends it to the list and then returns the whole list.
    The main reason is, I will use this particular function in other scripts to pass the processed summary of text files.

---
+++ run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import requests
+import os
+
+def process_txt():
+    directory_path="supplier-data/descriptions"
+    field=['name','weight','description','image_name']
+    process_dict_list=[]
+    for each_file in os.listdir(directory_path):
+        if ".txt" in each_file:
+            count=0
+            process_dict={}
+            with open("{}/{}".format(directory_path,each_file)) as file:
+                for line in file:
+                    if field[count]=='name' or field[count]=='description' :
+                        process_dict[field[count]]=line.strip('\n')
+                    if field[count]=='weight':
+                        process_dict['weight']=int(line.strip('\n').strip(' lbs'))
+                    if field[count]=='image_name':
+                        name,_=each_file.split('.')
+                        img=name+'.jpeg'
+                        process_dict['image_name']=img
+                    count+=1    
+            process_dict_list.append(process_dict)
+    return process_dict_list
+
+if __name__ == "__main__":
+    url="http://localhost/upload"
+    process_dict_list=process_txt()
+    for process_dict in process_dict_list:
+        response=requests.post(url,json=process_dict)
